641af2f205a9d1c89b9e18679f123daa6e562e80
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-09-02 14:40:25 -0500

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -3,6 +3,9 @@
 
 # importo las librerias necesarias para poder trabajar mi dataframe
 
+pip install plotly-express
+pip install streamlit
+
 import pandas as pd
 import numpy as np 
 import plotly.express as px #---- pip install plotly-express
