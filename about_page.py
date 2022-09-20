3151889e7fda4d69f98f3bc2774121540b63239e
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-09-19 19:09:34 -0500

    Content used in About the Project page

--- about_page.py
+++ about_page.py
@@ -13,16 +13,13 @@ def show_about_page():
     
     st.write("""### Original Data""")
     st.write('* This project started with a spreadsheet that gathers a year of information from different platforms, such as Facebook Ads, Google Ads, Hubspot and more with more than 46k observations. This Dataset was used to create visualizations in Marketing and Business analysis pages.')
-    st.write('* After crossing information using a pivot table, the number of observations reduced to 1,870, which is the Dataset I will be using for my modeling.')
-    st.write('* Most important feature for calculating assistants was the number of Eventbrite registrations with 0.9 Pearson Correlation Score.')
-    #st.write("* 9 regression models were used for this project being XGBoost Regressor the model with best performance on test data with 0.77 R2 score, which is the model that we can use in the Predict page.")
-    #st.write("* The plots shown on the Explore page are made with outliers except the Price for location plot.")
+    st.write('* After crossing information using a pivot table, the number of observations reduced to 1,890, which is the Dataset I will be using for my modeling.')
+    st.write('* Most important feature for calculating assistants was the number of Eventbrite registrations with 0.89 Pearson Correlation Score.')
+    st.write("* 9 regression models were used for this project being Random Forest Regressor the model with best performance on test data with 0.73 R2 score, which is the model that we can use in the Predict page.")
     
-    #st.write("""### Considerations for further versions""")
-    #st.write("* As with any data project, the more data that we have the better. Surely, with more than 517 houses we could build better models.")
-    #st.write("* We missed a better way to sectorialize the houses. A cluster was made in order to classify the houses as West, Mid-West, Mid-East and East but once they were included in the models they failed to improve these.")
-    #st.write("* Having more features like the materials of which the house is made or the time since the house was built could significantly improve the models.")
-    #st.write("* An Artificial Neural Network was implemented, however in didn't gave a better performance than previous models. As I'm currently studying TensorFlow and Deep Learning, in the future we could retry this ANN approach, however in the case still we don't get better results it won't be a surprise because [tree-based models are still better than neural nets when working with tabular data.](https://arxiv.org/abs/2207.08815)")
+    st.write("""### Considerations for further versions""")
+    st.write("* It's important to consider optimize our campaign labeling. I didn't use these labels because we have more than 60 different labels.")
+    st.write("* I would like to include new features such as sex, city and age. I think this could help to improve the model performance.")
     
     
     #st.write("I learned a lot and had some fun doing this project. Hope to look it again in the future with more knowledge in order to improve it. Thank you for reading this far and have fun using this app.")
