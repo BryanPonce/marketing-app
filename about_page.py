586b9091854274131b21ebcd06d3b1eeceb6aafd
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-09-20 19:28:40 -0500

    Content used in About the Project page

--- about_page.py
+++ about_page.py
@@ -15,14 +15,17 @@ def show_about_page():
     st.write('* This project started with a spreadsheet that gathers a year of information from different platforms, such as Facebook Ads, Google Ads, Hubspot and more with more than 46k observations. This Dataset was used to create visualizations in Marketing and Business analysis pages.')
     st.write('* After crossing information using a pivot table, the number of observations reduced to 1,890, which is the Dataset I will be using for my modeling.')
     st.write('* Most important feature for calculating assistants was the number of Eventbrite registrations with 0.89 Pearson Correlation Score.')
-    st.write("* 9 regression models were used for this project being Random Forest Regressor the model with best performance on test data with 0.73 R2 score, which is the model that we can use in the Predict page.")
+    st.write('* 9 regression models were used for this project being Random Forest Regressor the model with best performance on test data with 0.73 R2 score, which is the model that we can use in the Predict page.')
+    st.write('* I included important Data in the predict page that can help the user find realistic goals based on the last cycle of campaigns.')
     
     st.write("""### Considerations for further versions""")
     st.write("* It's important to consider optimize our campaign labeling. I didn't use these labels because we have more than 60 different labels.")
-    st.write("* I would like to include new features such as sex, city and age. I think this could help to improve the model performance.")
+    st.write('* I would like to include new features such as sex, city and age. I think this could help to improve the model performance.')
+    st.write("* The most important feature I would like to include is the connection to my platforms' APIs, to keep my Data updated and not fixed as it is Today.")
     
     
-    #st.write("I learned a lot and had some fun doing this project. Hope to look it again in the future with more knowledge in order to improve it. Thank you for reading this far and have fun using this app.")
-    #st.write("I'm always open to hear feedback and I would deeply appreciate it! You can contact me through [LinkedIn](https://www.linkedin.com/in/ivanverdugo-analytics/) or you can shoot me an email to jesusivan.vc21@gmail.com")
-    #st.write("You can see the full code required to do this app in its [GitHub repo.](https://github.com/IvanVC21/Tijuana-House-Prices)")
+    st.write("It's amazing to find out how much you actually know and how much you can build from all the knowledge you got in the last year of programming experience. I must say I'm surprised I was capable of creating this web app and I encourage you to learn a programming language that can help you create tools for your everyday tasks.")
+    
+    st.write('If you want to give me some feedback, you can always contact me in [Linked In](https://www.linkedin.com/in/bryan-fernando-ponce/) or via email: bryanponce93@yahoo.com')
+    st.write("You can see the full code required to do this app in its [GitHub repo.](https://github.com/BryanPonce/marketing-app)")
     
