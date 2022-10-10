e6981dd14e3a1fd916c73ea2e83af0878f65d69a
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-10-10 16:02:33 -0500

    Code for Analyze Call Center page

--- analyze_call_center.py
+++ analyze_call_center.py
@@ -117,7 +117,7 @@ def show_analyze_call_center():
     st.title('Dashboard All In One: Call Center :bar_chart:')
     st.markdown('##')
     
-    st.write('Note: The date range of this data is from September 8, 2021 to September 21, 2022') 
+    st.write('Note: The date range of this data is from September 8, 2021 to September 28, 2022') 
 
     # create variables for streamlit kpi section --------------------------------------------------------------------------------------------
 
