7c937f06ffebec14064491e3cd6e41a4e5bf270f
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-10-12 17:23:21 -0500

    Code for Analyze Marketing page

--- analyze_marketing.py
+++ analyze_marketing.py
@@ -51,8 +51,8 @@ df1=df[df['paid']==1]
 
 # create a pivot table to find new metrics in dataset
 
-df1= pd.pivot_table(data=df1,index=['paid','fuente_original','inicio_del_informe','batch','plataforma_ad','escuela',
-                'desglose_de_fuente_original_2'
+df1= pd.pivot_table(data=df1,index=['paid','fuente_original','campaign_name','inicio_del_informe','batch',
+                'plataforma_ad','escuela','desglose_de_fuente_original_2','kw_paid_search'
                 ],
                 values=['importe_gastado','leads','clics','leads_hubspot','registro_eb',
                 'asistio','ensayo','inscrito','ventas'
@@ -82,12 +82,12 @@ df1.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
 # reorder columns for easier dataset reading
 
 df1 = df1.reindex(columns=[
-   'paid','inicio_del_informe','batch','escuela','fuente_original','plataforma_ad',
-   'desglose_de_fuente_original_2','importe_gastado','clics',
-   'leads','cpl_campaigns','leads_hubspot','cpl_hubspot',
-   'registro_eb','costo_registro','asistio','costo_asistente',
-   'ensayo','costo_ensayo','inscrito','cac','cvr','cvr_paid',
-   'ventas','roas'
+   'paid','inicio_del_informe','batch','escuela',
+   'fuente_original','plataforma_ad','campaign_name','desglose_de_fuente_original_2',
+   'kw_paid_search','importe_gastado','clics','leads','cpl_campaigns',
+   'leads_hubspot','cpl_hubspot','registro_eb','costo_registro',
+   'asistio','costo_asistente','ensayo','costo_ensayo','inscrito',
+   'cac','cvr','cvr_paid','ventas','roas'
    ]
 )
 
@@ -113,6 +113,11 @@ def show_analyze_marketing():
         options= df1['plataforma_ad'].unique(),
         default= df1['plataforma_ad'].unique()
     )
+    campaign_s = st.sidebar.multiselect(
+        'Campaign Name',
+        options= df1['campaign_name'].unique(),
+        default= df1['campaign_name'].unique()
+    )
     semana_s = st.sidebar.multiselect(
         'Need to see a specific week?',
         options= df1['inicio_del_informe'].unique(),
@@ -121,7 +126,7 @@ def show_analyze_marketing():
 
     # ------- connect sidebar to dataframe ------
 
-    df_select = df1.query('batch == @batch_s & escuela == @escuela_s & plataforma_ad == @plataforma_s & inicio_del_informe == @semana_s')
+    df_select = df1.query('batch == @batch_s & escuela == @escuela_s & plataforma_ad == @plataforma_s & inicio_del_informe == @semana_s& campaign_name == @campaign_s')
 
     # select for streamlit the filtered dataset
 
@@ -436,8 +441,249 @@ def show_analyze_marketing():
     )
 
     st.write(alumns_vs_cac)
+    st.markdown('---')
+
+    # plot best google ads kw EB rate ----------------------------------------------------------------
+
+    # create a pivot table for our best performing kw
+
+    st.subheader("This plot shows the best performing Key Words in Google Ads from lead to Eventbrite Registration.")
+    st.write('This visualization works only with Google Ads Information, if your filter does not include this platform, plot will not work.')
+    st.write('Tip: Filter shorter time periods and by school you are analyzing, otherwise, the plot will be hard to read.')
+
+    values=['0']
+    best_eb_kw = df_select[df_select.kw_paid_search.isin(values) == False]
     
-        # hide streamlit style---------------
+    best_eb_kw= pd.pivot_table(data=best_eb_kw, 
+                    index=['kw_paid_search'
+                          ],
+                    values=['leads_hubspot','registro_eb'
+                           ],
+                    aggfunc={'leads_hubspot':np.sum,'registro_eb':np.sum,
+                            },
+                    fill_value=0
+    )
+
+    best_eb_kw= best_eb_kw.reset_index()
+    
+    # new variables for measured metrics
+
+    best_eb_kw['cvr_eb']= round(best_eb_kw['registro_eb']/ best_eb_kw['leads_hubspot'],2)
+
+    # now create the visualization ------------------------------ 
+
+    best_eb_rate = make_subplots(specs=[[{'secondary_y':True}]])
+
+    best_eb_rate.add_trace(
+        go.Scatter(
+            x= best_eb_kw['kw_paid_search'],
+            y= best_eb_kw['cvr_eb'],
+            name= 'Registration Rate'),
+            secondary_y=True
+    )
+    best_eb_rate.add_trace(
+        go.Bar(
+            x= best_eb_kw['kw_paid_search'],
+            y= best_eb_kw['registro_eb'],
+            name= 'Registrations'),
+            secondary_y= False
+    )
+    best_eb_rate.update_layout(
+        title='<b>Best Eventbrite Rate Keywords</b>',
+        xaxis_title='Keyword',
+        yaxis_title='Registrations',
+        plot_bgcolor='rgba(0,0,0,0)',
+        xaxis=(dict(showgrid=False)),
+    )
+    best_eb_rate.update_yaxes(
+        title_text='Registration Rate',
+        secondary_y=True
+    )    
+    best_eb_rate.update_yaxes(
+        title_text='Registrations',
+        secondary_y=False
+    )
+    best_eb_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y2', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=True
+    )
+    best_eb_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=False
+    )
+
+    st.write(best_eb_rate)
+    st.markdown('---')
+
+    # ----------------------------------------------------------------------------------------------------------------------------
+     
+    # plot best google ads kw Assist rate ----------------------------------------------------------------
+
+    # create a pivot table for our best performing kw
+
+    st.subheader("This plot shows the best performing Key Words in Google Ads from lead to assistant.")
+    st.write('This visualization works only with Google Ads Information, if your filter does not include this platform, plot will not work.')
+    st.write('Tip: Filter shorter time periods and by school you are analyzing, otherwise, the plot will be hard to read.')
+
+    values=['0']
+    best_as_kw = df_select[df_select.kw_paid_search.isin(values) == False]
+    
+    best_as_kw= pd.pivot_table(data=best_as_kw, 
+                    index=['kw_paid_search'
+                          ],
+                    values=['leads_hubspot','asistio'
+                           ],
+                    aggfunc={'leads_hubspot':np.sum,'asistio':np.sum,
+                            },
+                    fill_value=0
+    )
+
+    best_as_kw= best_as_kw.reset_index()
+    
+    # new variables for measured metrics
+
+    best_as_kw['cvr_as']= round(best_as_kw['asistio']/ best_as_kw['leads_hubspot'],2)
+
+    # now create the visualization ------------------------------ 
+
+    best_as_rate = make_subplots(specs=[[{'secondary_y':True}]])
+
+    best_as_rate.add_trace(
+        go.Scatter(
+            x= best_as_kw['kw_paid_search'],
+            y= best_as_kw['cvr_as'],
+            name= 'Assistance Rate'),
+            secondary_y=True
+    )
+    best_as_rate.add_trace(
+        go.Bar(
+            x= best_as_kw['kw_paid_search'],
+            y= best_as_kw['asistio'],
+            name= 'Assistants'),
+            secondary_y= False
+    )
+    best_as_rate.update_layout(
+        title='<b>Best Assistance Rate Keywords</b>',
+        xaxis_title='Keyword',
+        yaxis_title='Assistants',
+        plot_bgcolor='rgba(0,0,0,0)',
+        xaxis=(dict(showgrid=False)),
+    )
+    best_as_rate.update_yaxes(
+        title_text='Assistance Rate',
+        secondary_y=True
+    )    
+    best_as_rate.update_yaxes(
+        title_text='Assistants',
+        secondary_y=False
+    )
+    best_as_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y2', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=True
+    )
+    best_as_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=False
+    )
+
+    st.write(best_as_rate)
+    st.markdown('---')
+
+    # ---------------------------------------------------------------------------------------------------------------------------- 
+
+    # plot best google ads kw CVR ----------------------------------------------------------------
+
+    # create a pivot table for our best performing kw
+
+    st.subheader("This plot shows the best performing Key Words in Google Ads from lead to Alumn.")
+    st.write('This visualization works only with Google Ads Information, if your filter does not include this platform, plot will not work.')
+    st.write('Tip: Filter shorter time periods and by school you are analyzing, otherwise, the plot will be hard to read.')
+
+    values=['0']
+    best_kw = df_select[df_select.kw_paid_search.isin(values) == False]
+    
+    best_kw= pd.pivot_table(data=best_kw, 
+                    index=['kw_paid_search'
+                          ],
+                    values=['leads_hubspot','inscrito'
+                           ],
+                    aggfunc={'leads_hubspot':np.sum,'inscrito':np.sum,
+                            },
+                    fill_value=0
+    )
+
+    best_kw= best_kw.reset_index()
+    
+    # new variables for measured metrics
+
+    best_kw['cvr_kw']= round(best_kw['inscrito']/ best_kw['leads_hubspot'],2)
+
+    # now create the visualization ------------------------------ 
+
+    best_kw_rate = make_subplots(specs=[[{'secondary_y':True}]])
+
+    best_kw_rate.add_trace(
+        go.Scatter(
+            x= best_kw['kw_paid_search'],
+            y= best_kw['cvr_kw'],
+            name= 'Conversion Rate'),
+            secondary_y=True
+    )
+    best_kw_rate.add_trace(
+        go.Bar(
+            x= best_kw['kw_paid_search'],
+            y= best_kw['inscrito'],
+            name= 'Alumns'),
+            secondary_y= False
+    )
+    best_kw_rate.update_layout(
+        title='<b>Best Converting Keywords</b>',
+        xaxis_title='Keyword',
+        yaxis_title='Alumns',
+        plot_bgcolor='rgba(0,0,0,0)',
+        xaxis=(dict(showgrid=False)),
+    )
+    best_kw_rate.update_yaxes(
+        title_text='CVR',
+        secondary_y=True
+    )    
+    best_kw_rate.update_yaxes(
+        title_text='Alumns',
+        secondary_y=False
+    )
+    best_kw_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y2', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=True
+    )
+    best_kw_rate.update_yaxes(
+        rangemode='tozero', 
+        scaleanchor='y', 
+        scaleratio=1, 
+        constraintoward='bottom', 
+        secondary_y=False
+    )
+
+    st.write(best_kw_rate)
+    st.markdown('---')
+
+    # ----------------------------------------------------------------------------------------------------------------------------
+    
+    # hide streamlit style---------------
 
     hide_style= """
             <style>
