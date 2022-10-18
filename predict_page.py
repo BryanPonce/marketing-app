cbef652235ed2928bc7658cd0ca2b7034662b4fa
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-10-18 15:00:51 -0500

    Code for Predict page

--- predict_page.py
+++ predict_page.py
@@ -19,7 +19,7 @@ regressor = data["model"]
 
 def load_df():
 
-    url= 'https://raw.githubusercontent.com/BryanPonce/marketing-app/main/dataset_mkt_28_09.csv'
+    url= 'https://raw.githubusercontent.com/BryanPonce/marketing-app/main/dataset_mkt_05_10.csv'
     df =  pd.read_csv(url, encoding='ISO-8859-1')
 
     df.columns = df.columns.str.replace(' ', '_')
