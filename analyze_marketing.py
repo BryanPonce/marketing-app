0c9825d6a840bd67ff7a891eb2aa838e4e708ff1
Bryan Ponce <90662619+BryanPonce@users.noreply.github.com>
2022-10-18 15:04:09 -0500

    Code for Analyze Marketing page

--- analyze_marketing.py
+++ analyze_marketing.py
@@ -137,7 +137,7 @@ def show_analyze_marketing():
     st.title('Dashboard All In One: Marketing :bar_chart:')
     st.markdown('##')
     
-    st.write('Note: The date range of this data is from September 8, 2021 to September 28, 2022') 
+    st.write('Note: The date range of this data is from September 8th, 2021 to October 5th, 2022') 
 
     # create variables for streamlit kpi section
 
